c367d745c05fb42a7448a694e594678e3325f5ab
Livepurposefully <nimiko_chan@live.com>
2017-03-29 20:48:02 -0400

    trying to validate user input in the addCheeseForm using annotations and needed parameters, as well as applying necessary changes to the add.html template

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,11 +1,20 @@
 package org.launchcode.models;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Created by Engineer on 3/22/2017.
  */
 public class Cheese {
+
+    @NotNull
+    @Size(min=3, max=15)
     private String name;
+
+    @NotNull
     private String description;
+
     private int cheeseId;
     private static int nextId=1;
 
